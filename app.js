58cc94b2ee5736fe69b919d73d7a6272dec2e01e
Konan <kixeyems0@gmail.com>
2023-09-16 18:31:42 +0100

    added more routes and models

--- app.js
+++ app.js
@@ -4,21 +4,32 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const { Product } = require('./Models/product')
 require('dotenv').config()
+
 const port = 3000
 const api = process.env.API_URL
 
-const productRouter = require('./Router/products')
 
 // middleware 
 app.use(express.json()) 
 app.use(morgan('tiny'))
-app.use(`${api}/products`, productRouter)
 
+
+// routes
+
+const productRouter = require('./Router/products')
+const usersRouter = require('./Router/users')
+const categoriesRouter = require('./Router/categories')
+const ordersRouter = require('./Router/orders')
+ 
+app.use(`${api}/products`, productRouter)
+app.use(`${api}/orders`, ordersRouter)
+app.use(`${api}/categories`, categoriesRouter)
+app.use(`${api}/users`, usersRouter)
+
+// db connect 
 mongoose.connect(process.env.CONNECTION_STRING)
 .then(()=> console.log('connected to db'))
 .catch((err)=> console.log(err))
 
-
-
-
+//sever 
 app.listen(port, () => console.log(`server runnning on port ${port}`))
