1fd3e67ba119116d43b97c61dbe823a0dbe4879f
Konan <kixeyems0@gmail.com>
2023-09-18 21:24:30 +0100

    added bcrypt password hashing

--- Router/users.js
+++ Router/users.js
@@ -1,19 +1,27 @@
 const { User } = require("../Models/user")
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 router.get("/", async (req, res) => {
-  const UserList = await User.find()
+  const UserList =  await User.find().select("-passwordHash")
 
   if(!UserList) return res.status(500).send('no users found')
   res.send(UserList)
 })
 
+router.get('/:id', async (req, res)=> {
+  const user = await User.findById(req.params.id).select("-passwordHash")
+  if(!user) return res.status(500).send('there is no user with the given id')
+
+  res.status(200).send(user)
+}) 
+
 router.post("/", async(req, res)=> {
   let user = new User({
   name: req.body.name,
   email: req.body.email,
-  passwordHash: req.body.passwordHash,
+  passwordHash: bcrypt.hashSync(req.body.password, 10),
   phone: req.body.phone,
   isAdmin: req.body.isAdmin,
   street: req.body.street,
