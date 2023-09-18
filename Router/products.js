148451fc80ac52934c49797da97f14be16964176
Konan <kixeyems0@gmail.com>
2023-09-18 20:00:47 +0100

    added optional query param with filter

--- Router/products.js
+++ Router/products.js
@@ -8,7 +8,11 @@ const router = express.Router()
 
 
 router.get(`/`, async (req, res)=> {
-  const productList = await Product.find().populate('category')
+  let filter = {}
+
+  if(req.query.categories) filter = {category : req.query.categories.split(',')}
+
+  const productList = await Product.find(filter).populate('category')
 
   if(!productList) return res.status(500).send({success:false})
   res.send(productList)
