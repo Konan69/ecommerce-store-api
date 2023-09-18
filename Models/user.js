68953637cad0a72bdae5a54205aee48159e69b50
Konan <kixeyems0@gmail.com>
2023-09-18 20:06:23 +0100

    enabled virtuals for more models

--- Models/user.js
+++ Models/user.js
@@ -44,13 +44,13 @@ const userSchema = new mongoose.Schema({
 
 });
 
-// userSchema.virtual('id').get(function () {
-//     return this._id.toHexString();
-// });
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
 
-// userSchema.set('toJSON', {
-//     virtuals: true,
-// });
+userSchema.set('toJSON', {
+    virtuals: true,
+});
 
 exports.User = mongoose.model('User', userSchema)
 exports.userSchema = userSchema
