779516627e3d49001896d4e1b90dea773c870356
Konan <kixeyems0@gmail.com>
2023-09-20 15:45:13 +0100

    added register route and check for duplicate users

--- Router/users.js
+++ Router/users.js
@@ -56,4 +56,26 @@ router.post('/login' , async (req, res) => {
   } else { return res.status(400).send('Email or Password incorrect')}
 })
 
+router.post("/register", async(req, res)=> {
+  let user = await User.findOne({email: req.body.email})
+  if(user) return res.status(400).send('user already registered') 
+
+  user = new User({
+  name: req.body.name,
+  email: req.body.email,
+  passwordHash: bcrypt.hashSync(req.body.password, 10),
+  phone: req.body.phone,
+  isAdmin: req.body.isAdmin,
+  street: req.body.street,
+  apartment: req.body.apartment,
+  zip: req.body.zip,
+  city: req.body.city,
+  country: req.body.country
+  })
+  user = await user.save()
+
+  if(!user) return res.status(400).send('user cannot be created ')
+
+  res.send(user)
+})
 module.exports = router
