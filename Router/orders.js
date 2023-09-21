c2ad735829fd0689afc90191898b62bed22b043b
Konan <kixeyems0@gmail.com>
2023-09-21 14:25:29 +0100

    added api to delete order

--- Router/orders.js
+++ Router/orders.js
@@ -69,9 +69,24 @@ router.put('/:id',async (req, res)=> {
       { new: true}
   )
   if(!order)
-  return res.status(400).send('the order cannot be update!')
+  return res.status(400).send('the order cannot be updated!')
 
   res.send(order);
+})
+
+router.delete('/:id', (req, res)=>{
+  Order.findByIdAndRemove(req.params.id).then(async order =>{
+      if(order) {
+          await order.orderItems.map(async orderItem => {
+              await OrderItem.findByIdAndRemove(orderItem)
+          })
+          return res.status(200).json({success: true, message: 'the order is deleted!'})
+      } else {
+          return res.status(404).json({success: false , message: "order not found!"})
+      }
+  }).catch(err=>{
+     return res.status(500).json({success: false, error: err}) 
+  })
 })
 
 module.exports =router;
