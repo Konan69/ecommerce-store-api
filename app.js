7985719f75e9478cc5cd331203feaaf6aae04bda
Konan <kixeyems0@gmail.com>
2023-10-08 06:19:24 +0100

    added code to serve static files

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.options('*', cors)
 // middleware  
 app.use(express.json()) 
 app.use(morgan('tiny'))
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
 app.use(authJwt)
 // app.use(errorhandler)
 // routes
